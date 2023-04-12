587e19731f580b6d4790d576f048b30d7063bc18
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-04-12 20:07:30 -0300

    2.0

---
+++ 2.0 - POO basico/2.0 - POO basico/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2._0___POO_basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] idades = {12,45,15,23 };
+
+            foreach (int i in idades)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
---
+++ 2.0 - POO basico/Classes/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    internal class Carro
+    {
+        string Marca;
+        string Cor;
+        string Modelo;
+
+    }
+}
