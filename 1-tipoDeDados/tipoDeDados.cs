11407a5797641bb50489da8667234ab38d35c60d
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-04-12 14:30:13 -0300

    1.0

---
+++ 1-tipoDeDados/tipoDeDados.cs
@@ -0,0 +1,31 @@
+// 8 bits
+// 0 a 255
+byte myByte = 255;
+// -128 a 127
+sbyte myByte1 = 2;
+bool verdadeiro;
+
+// 16bits
+short numero;
+ushort numero;
+char nome ;
+
+// 32 bits
+uint numero;
+int numero;
+float numero;
+
+// 64 bits
+long numero;
+ulong numero;
+double numero;
+
+
+//128 bits
+
+decimal numero;
+
+
+//Constante
+
+const int valor = 1;
