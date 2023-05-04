755eb9393446050a06258e468400b0812da4be31
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-04 07:55:51 -0300

    4.0

---
+++ 4.0-Exception/Exception/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+
+            try
+            {
+                int numero = int.Parse(Console.ReadLine());
+            }
+            catch (Exception) {
+                Console.WriteLine("Nao digitou  valor correto");
+            }
+            finally { Console.WriteLine("Finally"); }
+
+            Console.ReadKey();
+        }
+    }
+}
