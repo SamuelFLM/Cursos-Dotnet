eff1525e730770345a10bf090ecee2de7e376d50
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-11 07:17:23 -0300

    ex_1

---
+++ Exercicios/Exercicios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var valores = new List<int>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                valores.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine($"X = {valores[0] + valores[1]}");
+        }
+    }
+}
