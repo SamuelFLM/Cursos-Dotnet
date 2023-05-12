4687108c36f886e8bd63a320f3bed80b68b91e2f
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-12 17:12:14 -0300

    ex

---
+++ Intermediario/Revisao POO/Pessoa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revisao_POO
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public string Sexo;
+        public DateTime DataNascimento;
+        public double PosicaoX, PosicaoY;
+
+        public Pessoa(string nome, string sexo, DateTime dataNascimento, double posicaoX, double posicaoY)
+        {
+            Nome = nome;
+            Sexo = sexo;
+            DataNascimento = dataNascimento;
+            PosicaoX = posicaoX;
+            PosicaoY = posicaoY;
+        }
+
+        public string ResumoPessoa() {
+
+            string resumo = $"Nome: {this.Nome}\nSexo: {Sexo}\nData Nascimento: {DataNascimento}\n";
+            return resumo;
+        }
+
+        public string GetPosicao()
+        {
+            return $"{PosicaoX}, {PosicaoY}";
+        }
+
+    }
+}
