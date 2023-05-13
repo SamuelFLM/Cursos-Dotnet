ceaa1d83a17c129cce9c536c2ab7911f5813b8ef
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-13 07:17:36 -0300

    ex

---
+++ Exercicios/Exercicio_06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] pesos = { 2, 3, 5 };
+            List<byte> valores = new List<byte>();
+
+            for (int i = 0; i < pesos.Length; i++)
+            {
+                valores.Add(byte.Parse(Console.ReadLine()));
+            }
+
+            float calculo = (float)((valores[0] * pesos[0]) + (valores[1] * pesos[1]) + (valores[2] *pesos[2])) / 10;
+
+            Console.WriteLine($"MEDIA =  {calculo}");
+        }
+    }
+}
---
+++ Exercicios/Exercicio_07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<short> valores = new List<short>();
+
+            for (int i = 0; i <= 3; i++)
+            {
+                valores.Add(short.Parse(Console.ReadLine()));   
+            }
+
+            short diferenca = (short)((valores[0] * valores[1]) - (valores[2] * valores[3]));
+
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+        }
+    }
+}
