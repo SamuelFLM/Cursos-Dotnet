bafc726d4a5b979026fb5d355a80a815d40fbe0f
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-14 07:13:07 -0300

    ex

---
+++ Exercicios/Exercicio_08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> valores = new List<string>();
+
+            for (int i = 0; i < 3; i++) { valores.Add(Console.ReadLine()); }
+            float salary = float.Parse(valores[1]) * float.Parse(valores[2]);
+
+            Console.WriteLine($"NUMBER = {valores[0]}");
+            Console.WriteLine($"SALARY = {salary.ToString("F2")}");
+        }
+    }
+}
