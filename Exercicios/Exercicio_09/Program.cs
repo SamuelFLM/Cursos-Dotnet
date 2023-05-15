c2dba2092358b6d95eb70b0d823228fbb12eedcb
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-15 13:48:20 -0300

    ex

---
+++ Exercicios/Exercicio_09/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> inputSalario = new List<float>();
+            string  nome = Console.ReadLine();
+
+            for (int i = 0; i <2; i++) { inputSalario.Add(float.Parse(Console.ReadLine())); }
+
+            float caluloSalarioBonus = (inputSalario[1] * 0.15f) + inputSalario[0];
+            Console.WriteLine($"TOTAL =  {caluloSalarioBonus.ToString("F2")}");
+        }
+    }
+}
---
+++ Exercicios/Exercicio_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,] valores = new float[2,3];
+
+            int contador = 0;
+
+            while (contador < 2) { 
+
+                for (int j = 0; j < 3; j++)
+                {
+                    valores[contador,j] = float.Parse(Console.ReadLine());
+                }
+
+                contador++;
+            }
+
+            float calculo = (valores[0,1] * valores[0,2]) + (valores[1,1] * valores[1,2]);
+
+            Console.WriteLine($"Valor a pagar: R$ {calculo.ToString("F2")}");
+        }
+    }
+}
