6541d15779d9b54c81b6dffa4f8f8218e3db5bc3
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-19 08:03:07 -0300

    att

---
+++ Exercicios/Exercicio_11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numeroPI = float.Parse(Math.PI.ToString("F5"));
+
+            float raio = float.Parse(Console.ReadLine());
+
+            float volume = (float)((4.0 / 3) * numeroPI * Math.Pow(raio, 3));
+
+            Console.WriteLine($"VOLUME = {volume.ToString("F3")}");
+        }
+    }
+}
