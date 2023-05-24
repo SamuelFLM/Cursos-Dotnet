f6a1bceff5fad83ec72069dcf8439f652ffb7dc4
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-24 07:59:27 -0300

    att

---
+++ Exercicios/Exercicio_15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var valores = new List<float>();
+
+            for (int i = 0; i < 4; i++) { valores.Add(float.Parse(Console.ReadLine())); }
+
+            float distancia = (float)Math.Sqrt((Math.Pow(valores[2] - valores[0], 2)  + Math.Pow(valores[3] - valores[1], 2)));
+
+            Console.WriteLine(distancia.ToString("F4"));
+        }
+    }
+}
---
+++ Exercicios/Exercicio_16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercicio_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte distancia = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{distancia / 2} minutos");
+        }
+    }
+}
