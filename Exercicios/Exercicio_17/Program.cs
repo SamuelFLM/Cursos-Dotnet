4259b865b6528f2c02c7a52d9212dd810643538b
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-25 11:20:32 -0300

    att

---
+++ Exercicios/Exercicio_17/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        byte[] tempoGastoEVelocidadeMedia = new byte[2];
+
+        for (int i = 0; i < tempoGastoEVelocidadeMedia.Length; i++) { tempoGastoEVelocidadeMedia[i] = byte.Parse(Console.ReadLine()); } 
+
+        float calculoGastoDeCombustivel = tempoGastoEVelocidadeMedia[1] * tempoGastoEVelocidadeMedia[0];
+
+        Console.WriteLine((calculoGastoDeCombustivel / 12).ToString("F3"));
+    }
+}
---
+++ Exercicios/Exercicio_18/Program.cs
@@ -0,0 +1,12 @@
+﻿
+byte[] cedulas = { 100, 50, 20, 10, 5, 2, 1 };
+
+
+short nota = short.Parse(Console.ReadLine());
+
+for (int i = 0; i < cedulas.Length; i++)
+{
+    Console.WriteLine($" {nota / cedulas[i]} NOTA(S) de R$ {cedulas[i]}");
+
+    nota %= cedulas[i];
+}
