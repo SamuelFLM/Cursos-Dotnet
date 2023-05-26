f531763b49d5a1cdb18c727ecf4383e05da58e43
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-26 11:26:25 -0300

    att

---
+++ Exercicios/Exercicio_19/Program.cs
@@ -0,0 +1,5 @@
+﻿
+int segundos = 556;
+int[] tempo = { 3600, 60};
+
+Console.WriteLine($"{segundos / tempo[0]}:{(segundos / tempo[1]) % 60}:{segundos % tempo[1]}");
---
+++ Exercicios/Exercicio_20/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+short[] idade = { 400, 800, 30 };
+
+for (int i = 0; i < idade.Length; i++)
+{
+    Console.WriteLine($@"
+    {(idade[i] / 365)} ano (s) 
+    {(idade[i] % 365) / 30} mes (s)
+    {(idade[i] % 365) % 30} dia (s)
+    ");
+}
