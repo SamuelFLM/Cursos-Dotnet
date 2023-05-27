c04249ac4d3115ff583a7e8ae38f423d2866fd1a
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-27 09:06:09 -0300

    Project

---
+++ Exercicios/Exercicio_21/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string valor = "576.73";
+byte[] notas = { 100, 50, 20, 10, 5, 2 };
+float[] moedas = { 1, 0.50f, 0.25f, 0.10f, 0.05f, 0.01f };
+
+
+string[] valorSeparado = valor.Split('.');
+
+short valorNota = short.Parse(valorSeparado[0]);
+float valorMoeda = 73/ 100f;
+
+Console.WriteLine("NOTAS: ");
+for (int i = 0; i < notas.Length; i++)
+{
+    Console.WriteLine($"{valorNota / notas[i]} nota (s) de R$ {(notas[i]).ToString("F2")}");
+    valorNota %= notas[i];
+}
+
+Console.WriteLine("MOEDAS: ");
+for (int i = 0; i < moedas.Length; i++) 
+{
+    Console.WriteLine($"{(int)(valorMoeda / moedas[i])} moeda (s) de R$ {(moedas[i]).ToString("F2")}");
+    valorMoeda %= moedas[i];
+}
---
+++ Exercicios/Exercicio_22/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+//byte[] valores = { 5, 6, 7, 8 };
+byte[] valores = { 2, 3, 2, 6 };
+
+if (valores[1] > valores[2] && valores[3] > valores[0])
+{
+    if ((valores[2] + valores[3]) > (valores[0] + valores[1]))
+    {
+        if (valores[2] > 0 && valores[3] > 0)
+        {
+            Console.WriteLine("Valores aceitos");
+        }
+    }
+}
+else
+{
+     Console.WriteLine("Valores nao aceitos");
+}
