4a5c93124f5088a569bfc449a6ad3b5ac5761282
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-05-28 07:28:58 -0300

    Project

---
+++ Exercicios/Exercicio_24/Program.cs
@@ -0,0 +1,13 @@
+﻿
+byte[] intervalo = { 25, 50, 75, 100 };
+
+double[] input = { 25.01, 25, 100, -25.02};
+
+for (int i = 0; i < intervalo.Length; i++)
+{
+    if (input[i] >= 0 &&input[i] <= intervalo[0]) { Console.WriteLine($"Intervalo [0, 25]"); }
+    else if (input[i] > intervalo[0] && input[i] <= intervalo[1]) { Console.WriteLine($"Intervalo [25, 50]"); }
+    else if (input[i] > intervalo[1] && input[i] <= intervalo[2]) { Console.WriteLine($"Intervalo [50, 75]"); }
+    else if (input[i] > intervalo[2] && input[i] <= intervalo[3]) { Console.WriteLine($"Intervalo [75, 100]"); }
+    if (input[i] < 0) { Console.WriteLine("Fora de intervalo"); } 
+}
