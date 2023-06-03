ad0ad30fb60b2fbcd6d90e1919670e337650b88d
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-06-03 16:59:19 -0300

    Project

---
+++ Exercicios/Exercicio_25/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Dictionary<short, float> lanche = new Dictionary<short, float>
+{
+    { 1, 4.00f },
+    { 2, 4.50f },
+    { 3, 5.00f },
+    { 4, 2.00f },
+    { 5, 1.50f }
+};
+
+short[] entrada = { 2, 3 };
+
+float calculo = lanche[entrada[0]] * entrada[1];
+
+Console.WriteLine($"Total: R$ {calculo.ToString("F2")}");
